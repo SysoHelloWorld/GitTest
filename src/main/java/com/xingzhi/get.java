7695d993463e4058a09f820c389c53f77f26ac23
Xingzhi <Xingzhi@cdut.cn>
2022-07-20 17:15:12 +0800

    first commit

---
+++ src/main/java/com/xingzhi/get.java
@@ -0,0 +1,11 @@
+package com.xingzhi;
+
+/**
+ * @author xingzhi
+ * @create 2022-07-20 17:11
+ */
+public class get {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
